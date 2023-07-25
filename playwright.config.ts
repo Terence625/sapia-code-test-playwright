a0d546dbdecf9e30b994ec59741e750a35a5b871
Wenguang Tian <terencetianterence@gmail.com>
2023-07-25 17:14:47 +1000

    CI retry once and timeout set to 3 mins

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,10 +11,10 @@ export default defineConfig({
   testMatch: "**/?(*.)+(spec|test).[tj]s?(x)",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
-  timeout: 300000,
+  timeout: 180000,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://fi.sandbox.sapia.ai/",
