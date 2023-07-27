3477dd832af88ec04d7efce53722de3b17cd098c
Wenguang Tian <terencetianterence@gmail.com>
2023-07-27 14:10:58 +1000

    remove tests on webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,10 +34,5 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
   ],
 })
